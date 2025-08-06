4a2abc0ce0bf368e2f1f906fbd485427baa2cb83
CathyF9600 <yuchun.feng@mail.utoronto.ca>
2025-08-06 13:03:03 +0800

    dataset.py no proprio

--- torchrun_abs_ee_cnt.sh
+++ torchrun_abs_ee_cnt.sh
@@ -5,18 +5,21 @@ export HF_ENDPOINT=https://hf-mirror.com
 export HF_HUB_DISABLE_XET=True
 export WANDB_API_KEY=56c323ace61a5076f5d8e92a91237607bbc362a7
 
-port=13548
+name=abs_ee_cnt_mlp6_no_proprio
+port=13535
 source /home/anaconda3/etc/profile.d/conda.sh
 conda deactivate
 conda activate em
 # pip install fastapi
 torchrun --nproc-per-node=2 --nnodes=1 --node-rank=0 --master-addr=localhost --master-port=$port train.py \
   --model_type continuous \
+  --decoder_name mlp_decoder_large \
   --batch-size 32 \
   --dim_actions 20 \
+  --num_actions 30 \
   --learning_rate 5e-4 \
   --precision bf16 \
   --port $port \
-  --output_dir runnings/RoboTwin/abs_ee_cnt_mlp-d3 \
-  --wandb_name robotwin2_abs_ee_cnt \
+  --output_dir runnings/RoboTwin/$name \
+  --wandb_name $name \
   --metas_path /home/fyc/EmpiricalStudyForVLA/datasets/meta_files/robotwin2_abs_ee.jsonl  
